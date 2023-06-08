23f82a87037cdc208ef94bdb61a85530818f6060
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-06-08 11:55:45 +0300

    feat: add api key

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b77f31a98069421ca069c84ddd8af020', // получите свой ключ https://newsapi.org/
         });
     }
 }
