dcf2b497c0ebb31835880dc0933a555ad9ea4438
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-06-10 19:39:32 +0300

    refactor: clean up remove commented code, fix error messages

--- migration-newip-to-ts/src/helpers/helpers.ts
+++ migration-newip-to-ts/src/helpers/helpers.ts
@@ -1,7 +1,7 @@
 export function getSafeElement<T extends HTMLElement>(selector: string, parentElement: DocumentFragment | Document): T {
     const elem = parentElement.querySelector<T>(selector);
     if (!elem) {
-        throw new Error(selector);
+        throw new Error('Element not found');
     }
     return elem;
 }
