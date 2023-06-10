94208e9c7da9f3fafc9b47e8f6a4d44600af72cf
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-06-10 20:09:00 +0300

    fix: add proxy for deployment

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '6e8f582f14fd4b4995103ef9270575e1', // получите свой ключ https://newsapi.org/
         });
     }
