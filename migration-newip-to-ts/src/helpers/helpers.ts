2ee3b127fe6b87232c749d9257032bd680f3454c
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-06-10 13:07:43 +0300

    style: change app layout, add styles

--- migration-newip-to-ts/src/helpers/helpers.ts
+++ migration-newip-to-ts/src/helpers/helpers.ts
@@ -1,7 +1,4 @@
-export function getSafeElement<T extends HTMLElement>(
-    selector: string,
-    parentElement: DocumentFragment | Document = document
-): T {
+export function getSafeElement<T extends HTMLElement>(selector: string, parentElement: DocumentFragment | Document): T {
     const elem = parentElement.querySelector<T>(selector);
     if (!elem) {
         throw new Error(selector);
