cd36ac4227ca17723fac2db703ad03b2f38a1bd1
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-06-11 19:05:48 +0300

    refactor: add readonly property to parameters

--- migration-newip-to-ts/src/components/view/select/select.ts
+++ migration-newip-to-ts/src/components/view/select/select.ts
@@ -3,10 +3,10 @@ import './select.css';
 export default class Select {
     public draw(optionsConfig: Record<string, string>, selectElement: HTMLSelectElement): void {
         const optionsArray: readonly [string, string][] = Object.entries(optionsConfig);
-        optionsArray.forEach((optionsItem: [string, string]) => this.addOptions(optionsItem, selectElement));
+        optionsArray.forEach((optionsItem: readonly [string, string]) => this.addOptions(optionsItem, selectElement));
     }
 
-    private addOptions(optionsItem: [string, string], selectElement: HTMLSelectElement): void {
+    private addOptions(optionsItem: readonly [string, string], selectElement: HTMLSelectElement): void {
         const option: HTMLOptionElement = document.createElement('option');
         option.innerText = optionsItem[0];
         option.value = optionsItem[1];
