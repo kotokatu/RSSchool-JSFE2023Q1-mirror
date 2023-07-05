0241f02d81482f155f03b5c8038e9a4d9455b607
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-07-05 13:59:46 +0300

    init: start async-race task

---
+++ async-race/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const EslintPlugin = require("eslint-webpack-plugin");
+
+const baseConfig = {
+  entry: path.resolve(__dirname, "./src/app"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      { test: /\.ts$/i, use: "ts-loader" },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].[chunkhash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      // favicon: path.resolve(__dirname, './src/favicon.ico'),
+      // filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: false,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new EslintPlugin({ extensions: "ts" }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
+};
